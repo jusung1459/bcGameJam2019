ec1337e58e0c9878c1ce561d080588747204e646
Crystal <crystalliang882@gmail.com>
2019-02-16 03:55:52 -0800

    resized whale

--- classes/playerClass.lua
+++ classes/playerClass.lua
@@ -8,26 +8,14 @@ local window_height = love.graphics.getHeight()
 
 function Player:new()
   self.image = love.graphics.newImage( "/art/whale.png" )
-  self.x = 300
-  self.y = 20
+  self.x = 40
+  self.y = 0
   self.speed = 500
   self.width = self.image:getWidth()
   self.height = self.image:getHeight()
 end
 
 function Player:update(dt)
-
-  if love.keyboard.isDown( "down" ) then
-    self.y = self.y + self.speed * dt
-  elseif love.keyboard.isDown( "up" ) then
-    self.y = self.y - self.speed * dt
-  elseif love.keyboard.isDown( "left" ) then
-    self.x = self.x - self.speed * dt
-  elseif love.keyboard.isDown( "right" ) then
-    self.x = self.x + self.speed * dt
-  end
-
-
   if self.x < 0 then self.x = 0
   elseif self.x + self.width > window_width then
     self.x = window_width - self.width
@@ -66,25 +54,14 @@ end
 
 function Son:new()
   self.image = love.graphics.newImage("art/penguin.png")
-  self.x = 300
-  self.y = 40
+  self.x = 0
+  self.y = 0
   self.speed = 500
   self.width = self.image:getWidth()
   self.height = self.image:getHeight()
 end
 
 function Son:update(dt)
-
-  if love.keyboard.isDown( "down" ) then
-    self.y = self.y + self.speed * dt
-  elseif love.keyboard.isDown( "up" ) then
-    self.y = self.y - self.speed * dt
-  elseif love.keyboard.isDown( "left" ) then
-    self.x = self.x - self.speed * dt
-  elseif love.keyboard.isDown( "right" ) then
-    self.x = self.x + self.speed * dt
-  end
-
   if self.x < 0 then self.x = 0
   elseif self.x + self.width > window_width then
     self.x = window_width - self.width
