b7c76b42a727e9eb3a0adb970ce9487fe5efe8a0
TJ <tjwu24@gmail.com>
2019-02-16 00:53:54 -0800

    menu

--- rooms/main_menu.lua
+++ rooms/main_menu.lua
@@ -3,7 +3,7 @@ Object = require 'libraries/classic/classic'
 Menu = Object:extend()
 
 function Menu:new()
-
+    background = love.graphics.newImage('art/menu')
 end 
 
 function Menu:update(dt)
@@ -11,5 +11,5 @@ function Menu:update(dt)
 end
 
 function Menu:draw()
-
+    love.graphics.draw(background, 0, 0)
 end
