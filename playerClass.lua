2cc7faaccc772cc25067eaff449d5cbf5636f0fb
Yuan Chen <yca316@sfu.ca>
2019-02-16 02:12:53 -0800

    constraint player within window

--- playerClass.lua
+++ playerClass.lua
@@ -3,14 +3,20 @@
 Player = Object:extend()
 Son = Object:extend()
 
+local window_width = love.graphics.getWidth()
+local window_height = love.graphics.getHeight()
+
 function Player:new()
   self.image = love.graphics.newImage( "/art/whale.png" )
   self.x = 300
   self.y = 20
   self.speed = 500
+  self.width = self.image:getWidth()
+  self.height = self.image:getHeight()
 end
 
 function Player:update(dt)
+
   if love.keyboard.isDown( "down" ) then
     self.y = self.y + self.speed * dt
   elseif love.keyboard.isDown( "up" ) then
@@ -19,6 +25,15 @@ function Player:update(dt)
     self.x = self.x - self.speed * dt
   elseif love.keyboard.isDown( "right" ) then
     self.x = self.x + self.speed * dt
+  end
+
+  if self.x < 0 then self.x = 0
+  elseif self.x + self.width > window_width then
+    self.x = window_width - self.width
+  end
+  if self.y < 0 then self.y = 0
+  elseif self.y + self.height > window_height then
+    self.y = window_height - self.height
   end
 end
 
@@ -31,9 +46,12 @@ function Son:new()
   self.x = 300
   self.y = 40
   self.speed = 500
+  self.width = self.image:getWidth()
+  self.height = self.image:getHeight()
 end
 
 function Son:update(dt)
+
   if love.keyboard.isDown( "down" ) then
     self.y = self.y + self.speed * dt
   elseif love.keyboard.isDown( "up" ) then
@@ -43,6 +61,16 @@ function Son:update(dt)
   elseif love.keyboard.isDown( "right" ) then
     self.x = self.x + self.speed * dt
   end
+
+  if self.x < 0 then self.x = 0
+  elseif self.x + self.width > window_width then
+    self.x = window_width - self.width
+  end
+  if self.y < 0 then self.y = 0
+  elseif self.y + self.height > window_height then
+    self.y = window_height - self.height
+  end
+
 end
 
 function Son:draw()
