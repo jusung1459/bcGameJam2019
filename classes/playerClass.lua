e47b1e206768a424b7b7c1b24647a72c9064c5dd
Yuan Chen <yca316@sfu.ca>
2019-02-16 14:40:26 -0800

    Boses

--- classes/playerClass.lua
+++ classes/playerClass.lua
@@ -1,10 +1,13 @@
 -- Player and Son class
 Player = Object:extend()
 Son = Object:extend()
+Box = Player:extend()
+
 
 local window_width = love.graphics.getWidth()
 local window_height = love.graphics.getHeight()
 
+--create a new player
 function Player:new()
   self.image = love.graphics.newImage( "/art/whale.png" )
   self.x = 40
@@ -14,11 +17,49 @@ function Player:new()
   self.height = self.image:getHeight()
 end
 
-function Player:update(dt)
+function Player:checkCollision(player, b)
+    --With locals it's common usage to use underscores instead of camelCasing
+    local player_left = self.x
+    local player_right = self.x + self.width
+    local player_top = self.y
+    local player_bottom = self.y + self.height
+
+    local b_left = b.x
+    local b_right = b.x + b.width
+    local b_top = b.y
+    local b_bottom = b.y + b.height
+
+    --If Red's right side is further to the right than Blue's left side.
+    if player_right > b_left and
+    --and Red's left side is further to the left than Blue's right side.
+    player_left < b_right and
+    --and Red's bottom side is further to the bottom than Blue's top side.
+    player_bottom > b_top and
+    --and Red's top side is further to the top than Blue's bottom side then..
+    player_top < b_bottom then
+        --There is collision!
+        return true
+    else
+        --If one of these statements is false, return false.
+        return false
+    end
+end
+
+--check the boundaries
+function Player:update(dt, stop)
+
+  if stop == true then
+    self.x = self.x - self.width
+    self.y = self.y - self.height
+    --[[self.x = self.width - self.x
+    self.y = self.height - self.y]]--
+    return false
+  end
   if self.x < 0 then self.x = 0
   elseif self.x + self.width > window_width then
     self.x = window_width - self.width
   end
+
   if self.y < 0 then self.y = 0
   elseif self.y + self.height > window_height then
     self.y = window_height - self.height
@@ -26,35 +67,52 @@ function Player:update(dt)
 
 end
 
+--draw the player
 function Player:draw()
   love.graphics.draw( self.image, self.x, self.y )
 end
 
+--move character+son
 function Player:keypressed(key, son)
-  if key == "a" then
-    self.x = 200
-    self.y = 200
-  end
+
   if key == "up" then
     son.y = self.y
     son.x = self.x
-    self.y = self.y - 40
+    if self.y ~= 0 then
+      if walls[current_level][(self.y/40)][(self.x/40)+1] == 0 then
+        self.y = self.y - 40
+      end
+    end
 
   end
+
   if key == "down" then
     son.y = self.y
     son.x = self.x
-    self.y = self.y + 40
+    if self.y ~= 560 then
+      if walls[current_level][(self.y/40)+2][(self.x/40)+1] == 0 then
+        self.y = self.y + 40
+      end
+    end
   end
+
   if key == "left" then
     son.y = self.y
     son.x = self.x
-    self.x = self.x - 40
+    if self.x ~= 0 then
+      if walls[current_level][(self.y/40)+1][(self.x/40)] == 0 then
+        self.x = self.x - 40
+      end
+    end
   end
   if key == "right" then
     son.y = self.y
     son.x = self.x
-    self.x = self.x + 40
+    if self.x ~= 760 then
+      if walls[current_level][(self.y/40)+1][(self.x/40)+2] == 0 then
+        self.x = self.x + 40
+      end
+    end
   end
 end
 
@@ -76,28 +134,74 @@ function Son:update(dt)
   elseif self.y + self.height > window_height then
     self.y  = window_height - self.height
   end
-
 end
 
 function Son:draw()
   love.graphics.draw( self.image, self.x, self.y )
 end
---
--- function Son:keypressed(key)
---   if key == "a" then
---     self.x = 200
---     self.y = 200
---   end
---   if key == "up" then
---     self.y = self.y - 40
---   end
---   if key == "down" then
---     self.y = self.y + 40
---   end
---   if key == "left" then
---     self.x = self.x - 40
---   end
---   if key == "right" then
---     self.x = self.x + 40
---   end
--- end
+
+function Box:new()
+  self.image = love.graphics.newImage("art/sand.png")
+  self.x = 200
+  self.y = 200
+  self.push = false
+  self.intact = false
+  self.width = self.image:getWidth()
+  self.height = self.image:getHeight()
+end
+
+function Box:draw()
+  love.graphics.draw( self.image, self.x, self.y )
+end
+function Box:update(dt, bool)
+  if bool == true then
+    self.intact = true
+  end
+end
+function Box:keypressed(key,son)
+  if self.intact == true then
+    if key == "a" then
+      self.push = not self.push
+    end
+    if self.push == true then
+      if key == "up" then
+        son.y = self.y
+        son.x = self.x
+        if self.y ~= 0 then
+          if walls[current_level][(self.y/40)][(self.x/40)+1] == 0 then
+            self.y = self.y - 40
+          end
+        end
+      end
+    end
+
+    if key == "down" then
+      son.y = self.y
+      son.x = self.x
+      if self.y ~= 560 then
+        if walls[current_level][(self.y/40)+2][(self.x/40)+1] == 0 then
+          self.y = self.y + 40
+        end
+      end
+    end
+
+    if key == "left" then
+      son.y = self.y
+      son.x = self.x
+      if self.x ~= 0 then
+        if walls[current_level][(self.y/40)+1][(self.x/40)] == 0 then
+          self.x = self.x - 40
+        end
+      end
+    end
+    if key == "right" then
+      son.y = self.y
+      son.x = self.x
+      if self.x ~= 760 then
+        if walls[current_level][(self.y/40)+1][(self.x/40)+2] == 0 then
+          self.x = self.x + 40
+        end
+      end
+    end
+  end
+end
--- game.lua
+++ game.lua
@@ -1,6 +1,6 @@
 Object = require 'libraries/classic/classic'
-require 'map'
 require "classes/playerClass"
+require "classes/volcano"
 
 Game = Object:extend()
 
@@ -8,9 +8,15 @@ function Game:new()
 
     player = Player()
     son = Son()
+    volcano = Volcano()
+    box = Box()
 
-    map = Map(1,1,tileblocks)
 
+    gear = love.graphics.newImage('art/menu/gear_wheel.png')
+    self:init()
+end
+
+function Game:init()
 end
 
 function Game:extend()
@@ -24,26 +30,49 @@ function Game:extend()
     cls.super = self
     setmetatable(cls, self)
     return cls
-  end
+end
 
   function Game:update(dt)
 
       player:update(dt)
       son:update(dt)
+      volcano:update(dt)
+      if volcano.halt == true then
+        collides = player:checkCollision(player, volcano)
+        if collides == true then
+          player:update(dt,true)
+        end
+      end
 
+      box:update(dt)
+      pushBox = box:checkCollision(player, box)
+      if pushBox == true then
+        box:update(dt, true)
+      end
+      self:update2(dt)
   end
+
+function Game:update2(dt)
+end
 
 function Game:draw()
 
-    map:draw()
+    self:draw2()
     player:draw()
     son:draw()
+    volcano:draw()
+    box:draw()
 
+    love.graphics.draw(gear, 0, 0)
+end
+
+function Game:draw2()
 end
 
 function Game:keypressed(key)
 
     player:keypressed(key, son)
+    box:keypressed(key,player)
     -- son:keypressed(key)
 
 end
@@ -51,7 +80,22 @@ end
 
 
 function Game:mousepressed(x, y, button, istouch)
+    if button == 1 then
+        if x >= 0 then
+            if y >= 0 then
+                if x <= 50 then
+                    if y <= 50 then
+                        gotoRoom('Options', 2)
+                    end
+                end
+            end
+        end
+    end
 end
+
+function Game:mousereleased(x, y, button, istouch)
+end
+
 
 function Game:activate()
 end
