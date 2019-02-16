0c86d8f1f8c25b170f8287f383facc47cb6b7c42
Yuan Chen <yca316@sfu.ca>
2019-02-16 00:45:41 -0800

    Adding functions

---
+++ playerClass.lua
@@ -0,0 +1,19 @@
+-- Player and Son class
+
+Player = Object:extend()
+Son = Object:extend()
+
+function Player:new()
+
+end
+
+function Player:draw()
+end
+
+
+function Son:new()
+
+end
+
+function Son:draw()
+end
