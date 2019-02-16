17203a5481ab1a27805086d550edbdff1f13ca87
Crystal <crystalliang882@gmail.com>
2019-02-16 09:54:13 -0800

    volcano

---
+++ classes/volcano.lua
@@ -0,0 +1,22 @@
+Object = require('libraries/classic/classic')
+
+Volcano = Object:extend()
+count = 0
+cross = love.graphics.newImage("/art/greenv.png")
+stop = love.graphics.newImage("/art/redv.png")
+
+function Volcano:new()
+  self.x = 0
+  self.y = 0
+end
+
+function Volcano:update(dt)
+  count = count + dt
+  if count > 3 then
+
+    count = 0
+  end
+end
+
+function Volcano:draw()
+  love.graphics.draw(drawable, x, y, r, sx, sy, ox, oy, kx, ky)
