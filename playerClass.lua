ff2152b12849d5ba876d08edd1ed88d17df252de
Yuan Chen <yca316@sfu.ca>
2019-02-16 02:19:08 -0800

    Player update

--- playerClass.lua
+++ playerClass.lua
@@ -1,7 +1,7 @@
 -- Player and Son class
 
 Player = Object:extend()
-Son = Object:extend()
+Son = Player:extend()
 
 local window_width = love.graphics.getWidth()
 local window_height = love.graphics.getHeight()
@@ -48,31 +48,4 @@ function Son:new()
   self.speed = 500
   self.width = self.image:getWidth()
   self.height = self.image:getHeight()
-end
-
-function Son:update(dt)
-
-  if love.keyboard.isDown( "down" ) then
-    self.y = self.y + self.speed * dt
-  elseif love.keyboard.isDown( "up" ) then
-    self.y = self.y - self.speed * dt
-  elseif love.keyboard.isDown( "left" ) then
-    self.x = self.x - self.speed * dt
-  elseif love.keyboard.isDown( "right" ) then
-    self.x = self.x + self.speed * dt
-  end
-
-  if self.x < 0 then self.x = 0
-  elseif self.x + self.width > window_width then
-    self.x = window_width - self.width
-  end
-  if self.y < 0 then self.y = 0
-  elseif self.y + self.height > window_height then
-    self.y = window_height - self.height
-  end
-
-end
-
-function Son:draw()
-  love.graphics.draw( self.image, self.x, self.y )
 end
