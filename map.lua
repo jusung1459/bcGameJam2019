4bd210e25c591d57f508bd8b7fd798e2ba512729
JuSung Lee <jusunglee1459@gmail.com>
2019-02-16 01:59:03 -0800

    map class

--- map.lua
+++ map.lua
@@ -2,8 +2,22 @@
 
 Map = Object:extend()
 
-function Map.new(width, height, tiles)
+function Map:new(width, height, tileblocks)
     self.width = width
     self.height = height
-    self.tiles = tiles
+    self.tileblocks = tileblocks
+end
+
+function Map:update()
+end
+
+function Map:draw()
+    for i,row in ipairs(self.tileblocks) do
+        for j,tile in ipairs(row) do
+            if tileblocks == 1 then
+                -- draw something
+                love.graphics.rectangle("fill", j*25, i*25, 20, 20)
+            end
+        end
+    end
 end
