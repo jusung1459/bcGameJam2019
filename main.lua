ad9c1592c22669685722414aeb0cc0d9a20099b6
JuSung Lee <jusunglee1459@gmail.com>
2019-02-15 23:58:14 -0800

    tileblocks

--- main.lua
+++ main.lua
@@ -1,4 +1,18 @@
 function love.load()
+
+    -- tileblocks
+    tileblocks = {  {1,1,1,0,1,0,1,0,1,0,1,0,1,0,1},
+                    {1,1,1,0,1,0,1,0,1,0,1,0,1,0,1},
+                    {1,1,1,0,1,0,1,0,1,0,1,0,1,0,1},
+                    {1,1,1,0,1,0,1,0,1,0,1,0,1,0,1},
+                    {1,1,1,0,1,0,1,0,1,0,1,0,1,0,1},
+                    {1,1,1,0,1,0,1,0,1,0,1,0,1,0,1} 
+                }
+
+
+
+
+
     image = love.graphics.newImage('image.png')
 end
 
@@ -7,5 +21,19 @@ function love.update(dt)
 end
 
 function love.draw()
+
+    -- draw tileblocks
+    function love.draw()
+        for i,row in ipairs(tileblocks) do
+            for j,tile in ipairs(row) do
+                if tile == 1 then
+                    -- draw something
+                    love.graphics.draw(image, 0, 0)
+                end 
+            end
+        end
+    end
+
+
     love.graphics.draw(image, 0, 0)
 end
