e3aafb0368e4d4f3b40547a1fee5a7337c5f98f3
Crystal <crystalliang882@gmail.com>
2019-02-16 10:22:43 -0800

    bc flamethrower wanted different tiles

--- classes/volcano.lua
+++ classes/volcano.lua
@@ -2,11 +2,8 @@ Object = require('libraries/classic/classic')
 
 Volcano = Object:extend()
 count = 0
-<<<<<<< HEAD
-=======
 cross = love.graphics.newImage("/art/greenv.png")
 stop = love.graphics.newImage("/art/redv.png")
->>>>>>> origin/master
 
 function Volcano:new()
   self.x = 0
@@ -14,25 +11,16 @@ function Volcano:new()
 end
 
 function Volcano:update(dt)
-<<<<<<< HEAD
   count = count + dt
-  if count > 3 then
-
-=======
-  count = count + 0.05
   if count > 3 then
     if whichimg == stop then
       whichimg = cross
     else whichimg = stop end
->>>>>>> origin/master
     count = 0
   end
 end
 
 function Volcano:draw()
-<<<<<<< HEAD
   love.graphics.draw(drawable, x, y, r, sx, sy, ox, oy, kx, ky)
-=======
   love.graphics.draw(whichimg, x, y)
 end
->>>>>>> origin/master
