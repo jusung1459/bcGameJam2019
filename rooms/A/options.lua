33a0bc4bc5c26b04d80b9381b3f336c7cd0c23b9
TJ <tjwu24@gmail.com>
2019-02-17 01:04:43 -0800

    -added option in the option menu to restart

--- rooms/A/options.lua
+++ rooms/A/options.lua
@@ -7,6 +7,7 @@ function Options:new()
     current_level = 2
     background = love.graphics.newImage('art/menu/background.png')
     picture = love.graphics.newImage('art/return.png')
+    suicide = love.graphics.newImage('art/menu.png')
 end
 
 function Options:update(dt)
@@ -15,6 +16,7 @@ end
 function Options:draw()
     love.graphics.draw(background, 0, 0)
     love.graphics.draw(picture, 300, 500)
+    if previous_room ~= "Menu" then love.graphics.draw(suicide, 300, 450) end
     love.graphics.print('nothing seems to be here!')
 end
 
@@ -22,7 +24,11 @@ function Options:mousepressed(x, y, button, istouch)
     if x >= 300 then
         if x <= 500 then
             if button == 1 then
-                if y >= 500 then
+                if y >= 450 then
+                    if y <= 490 then
+                        gotoRoom("Death", 0)
+                    end
+                elseif y >= 500 then
                     if y <= 540 then
                         gotoRoom(previous_room, previous_room_index)
                     end
