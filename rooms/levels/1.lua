71e74f19b8f185a097b28116d0e6eb68d240e602
Yuan Chen <yca316@sfu.ca>
2019-02-17 11:49:52 -0800

    fixed volcano bug

--- rooms/levels/1.lua
+++ rooms/levels/1.lua
@@ -160,61 +160,8 @@ function Lv1:update2()
 
 end
 function Lv1:draw2()
-
-    for i, row in ipairs (walls[current_level]) do
-        for j, tile in ipairs(row) do
-            -- if tile == 0 and i != 15 and j != 20 then
-            --   love.graphics.rectangle("fill", (j-1) * 40, (i-1) * 40, 40, 40)
-            -- end
-            -- else
-            --   love.graphics.rectangle("fill", j * 40, i * 40, 40, 40)
-            -- end
-            if tile == 0 then
-              -- if j == 1  or i == 1 then
-              --   love.graphics.draw(grass, (j-1) * 40, (i-1) * 40)
-              -- end
-              if j == 1 and i == 1 then
-                love.graphics.draw(topleft, 0, 0)
-
-              elseif j == 1 and i == 15 then
-                love.graphics.draw(botleft, (j-1)*40, (i-1)*40)
-
-              elseif j == 20 and i == 1 then
-                love.graphics.draw(topright, (j-1)*40, (i-1)*40)
-
-              elseif j == 20 and i == 15 then
-                love.graphics.draw(botright, (j-1)*40, (i-1)*40)
-
-              elseif i == 1 and j ~= 1 and j ~= 20 then
-                love.graphics.draw(top, (j-1)*40, (i-1)*40)
-
-              elseif j == 1 and i ~= 1 and i ~= 15 then
-                love.graphics.draw(left, (j-1)*40, (i-1)*40)
-
-              elseif j == 20  and i ~= 1 and i ~= 15 then
-                love.graphics.draw(right, (j-1)*40, (i-1)*40)
-
-              elseif i == 15 and j ~= 1 and j ~= 20 then
-                love.graphics.draw(bot, (j-1)*40, (i-1)*40)
-
-              elseif i ~= 1 and i ~= 15 and j ~= 1 and j ~= 20 then
-                love.graphics.draw(center, (j-1) * 40, (i-1) * 40)
-              end
-
-            end
-            if tile == 1 then
-              love.graphics.draw(wall, (j-1)*40, (i-1)*40)
-            end
-            -- love.graphics.draw(grass, j * 40, i * 40)
-            -- if tile == 1 then
-            --    love.graphics.draw(sand, (j-1) * 40, (i-1) * 40)
-            -- end
-        end
-    end
-
+  
     love.graphics.draw(topright, 760, 0)
-
-
     box:draw()
 
     volcano:draw()
